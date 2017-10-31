355555d36f0a2b2e85a6de755856d94e08035dc3
Gita Narasimhan <n.geeta@gmail.com>
2017-10-31 15:45:59 -0500

    Create constants.js

---
+++ frontend/src/actions/constants.js
@@ -0,0 +1,13 @@
+export const ADD_POST = 'ADD_POST'
+export const GET_POST = "GET_POST"
+export const GET_POST_ID = "GET_POST_ID"
+export const ADD_COMMENTS = "ADD_COMMENTS"
+export const GET_COMMENTS = "GET_COMMENTS"
+export const EDIT_COMMENT = "EDIT_COMMENT"
+export const DELETE_COMMENT = "DELETE_COMMENT"
+export const EDIT_POST = "EDIT_POST"
+export const DELETE_POST = "DELETE_POST"
+export const GET_ALL_POSTS = "GET_ALL_POSTS"
+export const CURRENT_CAT = "CURRENT_CAT"
+export const CURR_CATE_POST_COUNT = "CURR_CATE_POST_COUNT"
+export const CURR_POST = "CURR_POST"
